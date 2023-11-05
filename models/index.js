1f902e2f25a079e755277d2e862547c8fa336da5
Brandon Mullins <webdevmullins@gmail.com>
2023-11-04 21:42:33 -0500

    add models and routes

--- models/index.js
+++ models/index.js
@@ -1,20 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product')
+const Category = require('./Category')
+const Tag = require('./Tag')
+const ProductTag = require('./ProductTag')
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+	foreignKey: 'category_id'
+})
+Category.belongsTo(Product)
 
 // Categories have many Products
+Category.hasMany(Product, {
+	foreignKey: 'id'
+})
+Product.belongsTo(Category)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'ProductTag' })
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag' })
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+	Product,
+	Category,
+	Tag,
+	ProductTag
+}
